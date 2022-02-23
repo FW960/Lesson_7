7548296bc4e4762abc0ee4406682150f3c9e3256
FW960 <artuninp@gmail.com>
2022-02-24 03:27:51 +0800

    Редактировал Код Через ILdasm и декомпилировал его через DotPeek

---
+++ RedactedILdasmFile/ProgramNew.cs
@@ -0,0 +1,30 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Lesson_7.app.ProgramNew
+// Assembly: Lesson_7.app, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 07D8CB9A-6303-49E8-8905-274AC13013FA
+// Assembly location: C:\Users\windo\source\repos\Lesson_7\Lesson_7.app\Lesson_7.app\RedactedILdasmFile.exe
+
+using System;
+
+namespace Lesson_7.app
+{
+  internal class ProgramNew
+  {
+    private static void Main()
+    {
+      string str1 = "VeryUnpredictableSecretPasswordThatHaveBeenRedactedInILdasm";
+      Console.WriteLine("Enter password: ");
+      string str2 = Console.ReadLine();
+      if (str2 != str1)
+      {
+        Console.WriteLine("Welcome User.");
+      }
+      else
+      {
+        if (!(str2 == str1))
+          return;
+        Console.Write("Welcome Admin.");
+      }
+    }
+  }
+}
