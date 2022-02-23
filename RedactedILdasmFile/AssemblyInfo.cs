0b5e3a7597ad23b937f7b846394c0f5505b8f77f
FW960 <artuninp@gmail.com>
2022-02-24 03:27:51 +0800

    Редактировал Код Через ILdasm и декомпилировал его через DotPeek

---
+++ RedactedILdasmFile/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Lesson_7.app")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Lesson_7.app")]
+[assembly: AssemblyTitle("Lesson_7.app")]
+[assembly: AssemblyVersion("1.0.0.0")]
